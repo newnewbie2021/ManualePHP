c7a082ee84361e7dfc3358fc0456c3b8cb7952c7
newnewbie2021 <mabruz@gmail.com>
2021-10-20 14:48:54 +0200

    prima commit

---
+++ cap02/DaLitriaGalloni.php
@@ -0,0 +1,9 @@
+<?php
+echo "\n";
+$a = readline("Inserisci i litri: ");
+// $b = readline('Inserisci il 2° numero: ');
+$galloni = floatval($a) * 0.34;
+echo "\n";
+echo "I galloni corrispondenti a $a litri sono $galloni";
+echo "\n";
+echo "\n";
---
+++ cap02/Somma2numeri.php
@@ -0,0 +1,5 @@
+<?php
+$a = readline('Inserisci il 1° numero: ');
+$b = readline('Inserisci il 2° numero: ');
+$somma = floatval($a) + floatval($b);
+echo "La somma è $somma";
